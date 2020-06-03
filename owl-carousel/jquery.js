f97b33afa0d2e70dab7f9d46150310685a6a4ebd
Khansa <57318947+Khansasiraj@users.noreply.github.com>
2020-06-04 01:14:38 +0500

    first commit

---
+++ owl-carousel/jquery.js
@@ -0,0 +1,11 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:3,
+    margin:30,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+ 
+})
